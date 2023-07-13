c04057ba1bbc4ded3bfc178d5321a905e3200335
je488 <134580295+je488@users.noreply.github.com>
2023-07-13 17:03:55 +0900

    설명 추가

--- 깊이우선탐색_조합.py
+++ 깊이우선탐색_조합.py
@@ -1,7 +1,7 @@
 #L은 트리의 레벨 및 res의 인덱스, s는 반복문을 시작하는 숫자
 #s부터 n까지 가지가 뻗어나감
 #DFS를 호출할 때 현재 돌고있는 반복문의 i에 +1한 값을 s값으로 넘겨줌
-#cnt += 1에서 cnt를 지역변수로 인식하여 에러가 발생할 수 있으므로 global로 cnt가 전역변수임을 알려줌
+#cnt += 1에서 cnt를 지역변수로 인식하여 에러가 발생할 수 있으므로 global로 cnt가 전역변수임을 표시
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
