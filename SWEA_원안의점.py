4472330318555bfd36c3c4731c629bc2ce0766f1
je488 <134580295+je488@users.noreply.github.com>
2023-07-17 13:37:01 +0900

    설명 추가

--- SWEA_원안의점.py
+++ SWEA_원안의점.py
@@ -1,3 +1,7 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYcllbDqUVgDFASR(문제링크)
+#1사분면을 기준으로 격자점의 개수를 구한 뒤 * 4(단, (0,1),(2,0)과 같이 좌표축에 위치하거나 원점인 경우 제외)
+#(1,0)(-1,0)(0,1)(0,-1)과 같이 좌표축 위에 위치한 격자점의 개수는 n * 4
+#(0,0)도 원 안에 포함되므로 +1
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
