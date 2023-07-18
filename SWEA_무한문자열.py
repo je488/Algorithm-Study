dd42faf8c1933d2d0161b26df2cf65383bd7f813
je488 <134580295+je488@users.noreply.github.com>
2023-07-18 15:37:47 +0900

    설명 추가

--- SWEA_무한문자열.py
+++ SWEA_무한문자열.py
@@ -1,3 +1,7 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYP5JmsqcngDFATW(문제링크)
+#본연의 문자열을 비교했을 때 같으면 yes
+#각 문자열의 앞글자가 다르면 무한히 반복해도 같아질 수 없으므로 no
+#문자열의 길이가 다른 경우, 서로의 문자열 길이를 곱해서 문자열의 길이를 같게 만든 후 비교
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
