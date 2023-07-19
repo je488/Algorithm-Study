1542596504a5ff5e08c68fd53794e6a72ee8fb3c
je488 <134580295+je488@users.noreply.github.com>
2023-07-19 15:53:47 +0900

    설명 추가

--- SWEA_체스판위의룩배치.py
+++ SWEA_체스판위의룩배치.py
@@ -1,3 +1,8 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYOBfxwaAXsDFATW(문제링크)
+#조건을 만족하기 위해서는 각 행과 열마다 'O'가 한 개씩 존재해야함 ex)1행, 1열에 'O'가 한개씩 존재
+#주어진 체스판 배치를 입력받아 li를 생성하고 zip()을 이용하여 li의 행과 열을 바꾼 cli 생성
+#li와 cli에서 count()를 이용하여 한줄씩 'O'개수를 세고 1개가 아닐 경우, res를 'no'로 바꾸고 break
+#조건을 만족하면 res가 바뀌지 않으므로 'yes'를 출력하고 조건을 만족하지 않을 경우 'no'출력
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
