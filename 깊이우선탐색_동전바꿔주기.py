57c4e8cd126e60787dff505eb3105110f7490f3b
je488 <134580295+je488@users.noreply.github.com>
2023-07-25 14:10:50 +0900

    설명 추가

--- 깊이우선탐색_동전바꿔주기.py
+++ 깊이우선탐색_동전바꿔주기.py
@@ -1,3 +1,8 @@
+#L은 레벨 및 ct(동전금액)와 cn(개수)의 인덱스, sum은 동전으로 만드는 금액
+#동전의 금액별로 개수+1개의 가지로 뻗어나감(동전을 한개도 사용하지 않는 경우 포함)
+#DFS를 호출할 때 동전의 금액(ct[L])과 사용할 개수(i)를 곱한 뒤 sum에 더해서 호출
+#sum이 주어진 지폐의 금액(T)보다 커지면 return -> cut
+#cnt += 1에서 cnt를 지역변수로 인식해 에러 발생 -> global로 전역변수 표시
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
