c42e97a33d3d3858af529570424ad0cb09de9d43
je488 <134580295+je488@users.noreply.github.com>
2023-08-08 15:41:09 +0900

    설명 추가

--- SWEA_제곱팰린드롬수.py
+++ SWEA_제곱팰린드롬수.py
@@ -1,3 +1,7 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXO72aaqPrcDFAXS(문제링크)
+#a부터 b까지 i로 반복하며 i의 제곱근(s)을 구하고 정수인지 확인
+#제곱근이 정수면 i와 제곱근을 문자열로 변환하고 팰린드롬인지 확인
+#둘다 팰린드롬일 경우 제곱 팰린드롬 수 이므로 cnt += 1
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
