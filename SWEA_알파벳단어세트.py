a6cb58774622b301b1173589adecd5165138c6a7
je488 <134580295+je488@users.noreply.github.com>
2023-08-10 17:53:30 +0900

    설명 추가

--- SWEA_알파벳단어세트.py
+++ SWEA_알파벳단어세트.py
@@ -1,3 +1,8 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXAdrmW61ssDFAXq(문제링크)
+#L은 레벨 및 word의 인덱스, str은 단어를 합쳐서 만든 문자열
+#주어진 단어를 str에 더하는 경우와 더하지 않는 경우로 나눠 2개의 가지로 뻗어나감
+#L이 n이 되면 set()으로 str의 중복된 알파벳을 제거 -> 길이가 26(알파벳 소문자 개수)이면 res += 1
+#res += 1에서 res를 지역변수 인식하여 에러가 발생할 수 있음 -> global로 전역변수 표시
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
