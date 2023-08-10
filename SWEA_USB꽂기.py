06942ef2ae42954ff17d1f426b3f2aeff7172328
je488 <pje488@naver.com>
2023-08-10 16:08:31 +0900

    성공

---
+++ SWEA_USB꽂기.py
@@ -0,0 +1,12 @@
+import sys
+# sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+T = int(input())
+for tc in range(1, T+1):
+    p, q = map(float, input().split())
+    s1 = (1 - p) * q
+    s2 = p * (1 - q) * q
+    if s1 < s2:
+        print(f'#{tc} YES')
+    else:
+        print(f'#{tc} NO')
