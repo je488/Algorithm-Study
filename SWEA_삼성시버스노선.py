f9f11c47e58c33b8c6924a13dd968bbfabf49c35
je488 <134580295+je488@users.noreply.github.com>
2023-08-23 15:56:19 +0900

    설명 추가

--- SWEA_삼성시버스노선.py
+++ SWEA_삼성시버스노선.py
@@ -1,3 +1,7 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWczm7QaACgDFAWn(문제링크)
+#정류장은 총 5000개로 cnt의 인덱스는 정류장 번호, cnt의 값은 정류장을 지나가는 버스 노선의 개수
+#n개의 버스 노선 범위를 입력받고 a부터 b까지 반복하며 cnt의 정류장 번호에 해당하는 값에 +1
+#p개의 정류장 번호를 입력받고 cnt에서 정류장 번호에 해당하는 값을 찾아 res에 저장 후 한꺼번에 출력
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
