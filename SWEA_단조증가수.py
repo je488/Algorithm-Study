ee2a43318502111f1fdd97f28f865a5e43ba84f3
je488 <134580295+je488@users.noreply.github.com>
2023-08-28 14:13:42 +0900

    설명 추가

--- SWEA_단조증가수.py
+++ SWEA_단조증가수.py
@@ -1,3 +1,9 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWcPjEuKAFgDFAU4(문제링크)
+#n개의 수 중에서 2개씩 곱해 단조 증가하는 수인지 판별
+#판별하기 위해 2개씩 곱한 수를 문자열로 바꾸어 각 숫자의 자릿수가 증가하는 지 체크
+#하나라도 증가하지 않으면 break로 반복문 탈출 -> 단조 증가하는 수가 아님
+#break없이 for문이 끝까지 실행됐을 경우 단조 증가하는 수이므로 res에 저장
+#res의 길이가 0이면 단조 증가하는 수가 없으므로 -1 출력
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
