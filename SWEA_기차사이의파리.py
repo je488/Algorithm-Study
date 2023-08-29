8bb0548ec867be561160a65e1bbfae723d6b6240
je488 <134580295+je488@users.noreply.github.com>
2023-08-29 13:47:37 +0900

    설명 추가

--- SWEA_기차사이의파리.py
+++ SWEA_기차사이의파리.py
@@ -1,3 +1,10 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWajaTmaZw4DFAWM(문제링크)
+#두 기차가 충돌하면 파리가 죽으므로 두 기차가 충돌하기까지 걸린 시간 구하기 -> time
+#기차가 충돌하기까지 걸린 시간을 이용해 파리가 죽기 전까지 이동한 거리 구하기 -> res
+#거리 = 속력 * 시간
+#time = 기차 사이의 거리 / (A의 속력 + B의 속력)
+#res = 파리의 속력 * time
+#실행시간을 단축하기 위해 정답을 ans배열에 저장한 후 한꺼번에 출력
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
