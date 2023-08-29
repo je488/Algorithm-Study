2b0069874039f7ab217e0e60ca41d125a98b042c
je488 <134580295+je488@users.noreply.github.com>
2023-08-29 17:29:01 +0900

    설명 추가

--- SWEA_새샘이의735게임.py
+++ SWEA_새샘이의735게임.py
@@ -1,7 +1,7 @@
 #https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWZ2IErKCwUDFAUQ(문제링크)
 #서로 다른 7개의 정수 중에서 3개의 정수를 순서 상관없이 고르기 -> 조합
-#중복을 허용하지 않으므로 i는 0부터, j는 i+1부터, k는 j+1부터 시작
 #3중 for문을 이용하여 3개의 정수를 뽑아 더한 값을 res에 저장
+#중복을 허용하지 않으므로 i는 0부터, j는 i+1부터, k는 j+1부터 시작
 #더한 값이 중복될 수 있으므로 res를 set으로 생성하여 중복 제거
 #5번째 큰 수 -> res를 내림차순으로 정렬한 후 4번 인덱스의 값
 #set은 인덱스로 접근이 불가능하나 sorted로 정렬한 후 리스트로 반환되므로 인덱스로 접근 가능
