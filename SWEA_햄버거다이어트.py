4ea8cc20f140afb275549d2d931d3e1ea2fb7c67
je488 <134580295+je488@users.noreply.github.com>
2023-09-04 15:29:56 +0900

    설명 추가

--- SWEA_햄버거다이어트.py
+++ SWEA_햄버거다이어트.py
@@ -1,3 +1,9 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWT-lPB6dHUDFAVT(문제링크)
+#L은 트리의 레벨 및 info의 인덱스, sum은 맛에 대한 점수의 합, cal은 칼로리의 합
+#각 재료를 햄버거에 포함시키는 경우와 포함시키지 않는 경우로 나눠 2개의 가지로 뻗어나감
+#칼로리의 합(cal)이 주어진 제한 칼로리보다 커지는 경우 return -> cut
+#sum이 기존에 구한 점수의 최댓값(res)보다 크면 res값을 sum으로 바꿔주기
+#res = sum에서 res를 지역변수로 인식 -> if sum > res에서 에러 발생 -> global로 전역변수 표시
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
