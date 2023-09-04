b72a64deb4152d624692e49d2281164212891e1e
je488 <134580295+je488@users.noreply.github.com>
2023-09-04 13:56:32 +0900

    설명 추가

--- SWEA_의석이의세로로말해요.py
+++ SWEA_의석이의세로로말해요.py
@@ -1,3 +1,8 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWVWgkP6sQ0DFAUO(문제링크)
+#리스트 li에 문자열 5개를 입력받아 저장
+#i는 하나의 문자열에서 각각의 문자에 해당하는 인덱스, j는 li에서 각각의 문자열에 해당하는 인덱스
+#문자열의 길이는 서로 다를 수 있고 최대 길이는 15이므로 0부터 14까지 반복
+#i가 문자열의 길이보다 작은 경우 즉, 문자열에서 i번째 문자가 있는 경우에만 res에 문자 저장
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
