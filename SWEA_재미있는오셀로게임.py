0ef7342fb0ef2cc5288123405b5c5e40e255d075
je488 <134580295+je488@users.noreply.github.com>
2023-09-06 16:26:29 +0900

    설명 추가

--- SWEA_재미있는오셀로게임.py
+++ SWEA_재미있는오셀로게임.py
@@ -1,3 +1,14 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWQmA4uK8ygDFAXj(문제링크)
+#돌을 놓을 때마다 나와 같은색 돌 사이에 있는 다른색 돌들을 모두 뒤집기
+#돌을 놓을 위치와 색이 주어지면 상하좌우와 대각선을 탐색하며 뒤집을 돌들을 찾아 뒤집기
+#dx, dy는 board에서 →, ↓, ←, ↑, ↗, ↘, ↙, ↖ 방향을 탐색하기 위한 행과 열의 인덱스 증가치
+#change는 뒤집어야 할 돌들의 위치를 저장한 리스트
+#상하좌우 및 대각선으로 탐색중인 좌표가 경계선을 넘는 경우 change 리스트를 비우고 break
+#좌표에 해당하는 돌이 같은색이면 좌표 전까지 change에 저장된 돌들만 뒤집으면 되므로 break
+#좌표가 0으로 비어있는 경우 change 리스트를 비우고 break
+#좌표가 경계선을 넘지 않고 비어있지 않고 같은색이 아니면 뒤집어야할 돌이므로 change에 저장
+#while 반복문이 종료되면 change에 저장된 위치에 해당하는 돌의 색을 주어진 색(c)으로 뒤집기
+#board를 탐색하며 흑돌의 개수(b_cnt)와 백돌의 개수(w_cnt) 구하기
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
@@ -42,5 +53,3 @@ for tc in range(1, T+1):
         b_cnt += b.count(1)
         w_cnt += b.count(2)
     print(f'#{tc} {b_cnt} {w_cnt}')
-
-
