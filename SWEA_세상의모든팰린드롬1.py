17b6a2d0ad1b44a11bead3578976456546e0907d
je488 <134580295+je488@users.noreply.github.com>
2023-09-07 15:57:23 +0900

    설명 추가

--- SWEA_세상의모든팰린드롬1.py
+++ SWEA_세상의모든팰린드롬1.py
@@ -1,7 +1,7 @@
 #https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWO6Oao6N4QDFAWw(문제링크)
 #주어진 문자열 s를 슬라이싱을 이용해 뒤집었을 때 같으면 팰린드롬이므로 Exist
 #같지 않은 경우 for문으로 s를 탐색하며 문자가 '?'이면 s에서 대칭되는 문자로 바꿔줌
-#모두 탐색하고 바꿔준 뒤에 슬라이싱을 이용해 뒤집었을 때 같으면 팰린드롬이고 다르면 팰린드롬X
+#모두 탐색하고 바꿔준 뒤에 슬라이싱을 이용하여 뒤집었을 때 같으면 팰린드롬이고 다르면 팰린드롬X
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
