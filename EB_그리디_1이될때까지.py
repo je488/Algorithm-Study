492144091de858637f9f4852d9e44e5b4dacf98d
je488 <pje488@naver.com>
2023-09-12 17:25:17 +0900

    성공

---
+++ EB_그리디_1이될때까지.py
@@ -0,0 +1,15 @@
+import sys
+# sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+n, k = map(int, input().split())
+res = 0
+while True:
+    target = (n // k) * k
+    res += (n - target)
+    n = target
+    if n < k:
+        break
+    res += 1
+    n //= k
+res += (n - 1)
+print(res)
