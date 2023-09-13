dc4c81c756442eeba5f85dc8390c7fc9e51a620d
je488 <134580295+je488@users.noreply.github.com>
2023-09-13 15:52:17 +0900

    설명 추가

--- SWEA_항구에들어오는배.py
+++ SWEA_항구에들어오는배.py
@@ -1,3 +1,10 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWMedCxalW8DFAXd(문제링크)
+#최소한의 배로 주어진 즐거운 날들의 목록(happy)을 만들 수 있어야 함
+#따라서 각 배들의 주기를 구하고 주기를 기준으로 배가 들어오는 날을 구했을 때 happy와 같아져야 함
+#주기의 최소 개수가 배의 최소 개수와 같음
+#happy의 두번째 값부터 ship에 없는 경우에만 1을 빼서 주기(tmp) 구하기
+#구한 주기를 기준으로 1일차를 제외하고 마지막 즐거운 날까지 배가 들어온 날을 ship에 추가
+#새로운 주기를 계산할 때마다 배의 개수(res)가 1씩 증가
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
