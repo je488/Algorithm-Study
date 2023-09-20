a37715e80aa8e6113e6d76589558c7e1b62d7a30
je488 <134580295+je488@users.noreply.github.com>
2023-09-20 13:40:18 +0900

    설명 추가

--- EB_구현_왕실의나이트.py
+++ EB_구현_왕실의나이트.py
@@ -1,3 +1,8 @@
+#이것이 취업을 위한 코딩테스트다 - 117p
+#x, y에 현재 나이트의 위치 입력 받기
+#x의 경우 a부터 h로 주어지므로 ord함수를 이용하여 숫자로 바꿔주기
+#steps는 나이트가 이동할 수 있는 8가지 방향의 행과 열의 증가치
+#xx, yy는 이동하려는 위치의 좌표로 각각 1과 8사이이면 이동이 가능하므로 res += 1
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
