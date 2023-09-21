033190b1d3fd31612a1f19ef91914682e4b02e47
je488 <134580295+je488@users.noreply.github.com>
2023-09-21 14:30:45 +0900

    설명 추가

--- SWEA_화섭이의정수나열.py
+++ SWEA_화섭이의정수나열.py
@@ -1,3 +1,9 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWHz7xD6A20DFAVB(문제링크)
+#n개의 정수가 공백 또는 줄바꿈으로 구분되어 있음
+#따라서 s의 길이가 n이 될 때까지 문자열로 입력받고 replace() 함수를 이용하여 공백 제거
+#res는 주어진 정수열(s)로 만들 수 없는 0 이상의 가장 작은 정수
+#res를 0으로 초기화하고 문자로 바꾼 뒤 s에 포함되면 주어진 정수열로 만들 수 있으므로 s += 1
+#res가 s에 포함되어 있지 않다면 만들 수 없는 정수이므로 정답
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
