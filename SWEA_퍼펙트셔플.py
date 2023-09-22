a9102579ec92aacfa0ffff5ab568574105c14264
je488 <134580295+je488@users.noreply.github.com>
2023-09-22 15:26:19 +0900

    설명 추가

--- SWEA_퍼펙트셔플.py
+++ SWEA_퍼펙트셔플.py
@@ -1,3 +1,8 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWGsRbk6AQIDFAVW(문제링크)
+#l은 cards를 반으로 나누었을 때 첫 번째 카드 그룹을 나타내는 인덱스
+#r은 cards를 반으로 나누었을 때 두 번째 카드 그룹을 나타내는 인덱스
+#n을 2로 나눈 몫만큼 반복해서 res에 카드를 교대로 저장하고 l과 r을 1씩 증가시키기
+#n이 홀수이면 마지막에 cards의 중간에 위치한 cards[n//2]를 res에 저장
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
