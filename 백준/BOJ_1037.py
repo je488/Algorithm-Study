687cb1847c037253e1d2a92b24187e3cabf83c1a
je488 <134580295+je488@users.noreply.github.com>
2024-01-03 12:54:52 +0900

    설명 추가

--- 백준/BOJ_1037.py
+++ 백준/BOJ_1037.py
@@ -1,3 +1,7 @@
+#divisor는 N의 진짜 약수가 모두 담긴 리스트
+#따라서 divisor의 최소값과 최대값을 곱해주면 N
+#시간복잡도는 N의 진짜 약수가 M개라고 할 때 O(M+M) = O(2M) = O(M)
+#만약 N의 진짜 약수가 일부만 주어졌을 경우 최소값 * 최대값으로 풄 수 없음
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
