521e6fbbac17197d8e6ce53a60101544ed3bfbb1
je488 <pje488@naver.com>
2024-01-08 14:19:18 +0900

    성공

---
+++ 백준/BOJ_17425.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+MAX = 1000000
+d = [1] * (MAX+1)
+s = [0] * (MAX+1)
+for i in range(2, MAX+1):
+    j = 1
+    while i * j <= MAX:
+        d[i*j] += i
+        j += 1
+for i in range(1, MAX+1):
+    s[i] = s[i-1] + d[i]
+T = int(input())
+ans = []
+for _ in range(T):
+    n = int(input())
+    ans.append(s[n])
+print('\n'.join(map(str, ans)) + '\n')
