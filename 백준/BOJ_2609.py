21a0610d7170de3fcd56638bc56ecda26770501e
je488 <134580295+je488@users.noreply.github.com>
2024-01-08 15:44:37 +0900

    설명 추가

--- 백준/BOJ_2609.py
+++ 백준/BOJ_2609.py
@@ -1,3 +1,8 @@
+#gcd는 최대공약수, lcm은 최소공배수
+#최대공약수는 유클리드 호제법을 이용하여 구하기
+#a % b가 r일 때 GCD(a, b) = GCD(b, r)이며 r이 0이면 그 때의 b가 최대 공약수
+#재귀함수를 이용하여 b가 0이면 a를 리턴하고 그렇지 않으면 위의 식에 따라 GCD(b, a % b)를 리턴
+#최소공배수는 gcd * (a / gcd) * (b / gcd) = a * b / gcd
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
