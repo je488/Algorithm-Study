aba12050223441c22f4fe1263aa5b33790a4a3e1
je488 <134580295+je488@users.noreply.github.com>
2024-01-10 11:55:30 +0900

    설명 추가

--- 백준/BOJ_2309_2.py
+++ 백준/BOJ_2309_2.py
@@ -1,3 +1,7 @@
+#BOJ_2309_1.py보다 빠르게 푸는 방법
+#난쟁이의 키는 변하지 않으므로 9명의 키의 합을 total에 저장
+#i, j로 2명을 고르면 total - a[i] - a[j]는 나머지 7명의 키의 합 -> 시간복잡도 O(1)
+#따라서 총 시간복잡도는 O(N^2 * 1) = O(N^2)
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
