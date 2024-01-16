e8905387e87d6339978f72b6f12ff6f04a2d0c6d
je488 <134580295+je488@users.noreply.github.com>
2024-01-16 16:13:42 +0900

    설명 추가

--- 백준/BOJ_1476_1.py
+++ 백준/BOJ_1476_1.py
@@ -1,3 +1,6 @@
+#가능한 연도의 개수는 15 * 28 * 19 = 7980으로 모든 경우 다 해보기
+#e, s, m을 1씩 더해주다가 각각 16, 29, 20이 되면 1로 값을 바꿔주기
+#year도 1씩 더해주다가 e == E이고 s == S이고 m == M이면 그 때의 year 출력
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
