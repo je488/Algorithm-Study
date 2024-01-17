a5b7f3f077303036f6d15bd28322b75d9307baa9
je488 <134580295+je488@users.noreply.github.com>
2024-01-17 15:36:51 +0900

    설명 추가

--- 백준/BOJ_18290_4.py
+++ 백준/BOJ_18290_4.py
@@ -1,3 +1,9 @@
+#2차원 리스트 a를 1차원 리스트라고 가정해서 문제 풀기
+#(i, j)는 i * m + j로 표현 -> row-major order
+#ex) a가 3*4인 경우 a[0][0] -> 0, a[0][1] -> 1, ..., a[1][0] -> 4, ..., a[2][3] -> 11
+#반복문에서 j는 prev + 1(이전에 선택한 칸 + 1)부터 시작하고 n * m 전까지
+#칸의 x좌표는 j // m, y좌표는 j % m
+#재귀 함수 호출 시, 현재 칸의 좌표를 1차원 리스트의 좌표처럼 표현한 x * m + y를 인자로 넘기기
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
