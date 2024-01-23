216095d5214000057478656abff9852195f11b8f
je488 <134580295+je488@users.noreply.github.com>
2024-01-23 14:56:59 +0900

    설명 추가

--- 백준/BOJ_10974.py
+++ 백준/BOJ_10974.py
@@ -1,3 +1,7 @@
+#순열을 만들어서 출력하고 다음 순열을 구하는 함수(next_permutation) 호출
+#next_permutation 함수가 False를 리턴할 때까지(다음 순열이 없을 때까지) 반복
+#첫 순열은 1 ~ N을 오름차순(비내림차순)으로 정렬해서 구하기
+#시간 복잡도는 다음 순열(O(N))의 연산을 순열의 개수(N!)만큼 해야하므로 O(N * N!)
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
