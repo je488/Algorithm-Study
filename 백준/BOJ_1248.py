b388c760ebe7e6ea7fecf831d3e4efd6f773a423
je488 <134580295+je488@users.noreply.github.com>
2024-01-25 15:35:56 +0900

    설명 추가

--- 백준/BOJ_1248.py
+++ 백준/BOJ_1248.py
@@ -1,3 +1,11 @@
+#s가 하나의 문자열로 주어지므로 2차원 리스트(sign)로 변경
+#sign[i][i]는 i번째 수의 부호
+#따라서 sign[i][i]가 0보다 크면 1 ~ 10, 0보다 작으면 -10 ~ -1, 0이면 0을 ans[i]에 저장
+#i번째 수를 결정하면 sign[j][i](0 <= j < i)꼴의 부호를 모두 검사할 수 있음
+#따라서 수열을 만든 뒤에 부호가 맞는지 체크하지 않고 수를 하나씩 결정할 때 부호가 맞는지 체크
+#check 함수는 index번째 수로 끝나는 모든 합(s)을 구해서 s의 부호가 입력과 같은 지 체크
+#모든 합의 부호가 입력과 같으면 True, 다르면 False 리턴
+#check 함수가 True를 리턴하면 재귀 함수(go)를 호출하고 False를 리턴하면 함수 호출 종료
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
