a7251d073fbc62c7b5e5857aba44a283c922473f
je488 <pje488@naver.com>
2024-02-06 15:02:46 +0900

    성공

---
+++ 백준/BOJ_2193.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+d = [0] * 91
+n = int(input())
+d[1] = 1
+d[2] = 1
+for i in range(3, n+1):
+    d[i] = d[i-1] + d[i-2]
+print(d[n])
