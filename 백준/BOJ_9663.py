57f144199047790e9493be34ec11ee3e575dafde
je488 <pje488@naver.com>
2024-02-26 12:35:20 +0900

    성공

---
+++ 백준/BOJ_9663.py
@@ -0,0 +1,35 @@
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+def check(row, col):
+    if check_col[col]:
+        return False
+    if check_dig[row+col]:
+        return False
+    if check_dig2[n-1+row-col]:
+        return False
+    return True
+
+def go(row):
+    if row == n:
+        return 1
+    ans = 0
+    for col in range(n):
+        if check(row, col):
+            check_col[col] = 1
+            check_dig[row+col] = 1
+            check_dig2[n-1+row-col] = 1
+            a[row][col] = 1
+            ans += go(row+1)
+            check_col[col] = 0
+            check_dig[row+col] = 0
+            check_dig2[n-1+row-col] = 0
+            a[row][col] = 0
+    return ans
+
+n = int(input())
+a = [[0] * n for _ in range(n)]
+check_col = [0] * n
+check_dig = [0] * (2*n-1)
+check_dig2 = [0] * (2*n-1)
+print(go(0))
