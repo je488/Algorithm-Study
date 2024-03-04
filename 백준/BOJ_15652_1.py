1209d3bdf948dbb898dc8a16518f72f40f44e96a
je488 <134580295+je488@users.noreply.github.com>
2024-03-04 15:27:25 +0900

    설명 추가

--- 백준/BOJ_15652_1.py
+++ 백준/BOJ_15652_1.py
@@ -1,3 +1,6 @@
+#BOJ_15649.py에 BOJ_15650.py와 BOJ_15651.py의 조건을 추가한 문제
+#숫자를 중복해서 선택할 수 있으므로 idx번째의 숫자가 i면 idx+1번째에 올 수 있는 수는 i ~ n
+#따라서 idx번째의 숫자를 i로 결정한 뒤에 go(idx+1, i) 호출
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
