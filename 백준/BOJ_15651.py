b8646aa5b443d9d40970e4cbfa48c0bd38c91441
je488 <134580295+je488@users.noreply.github.com>
2024-03-04 15:14:51 +0900

    설명 추가

--- 백준/BOJ_15651.py
+++ 백준/BOJ_15651.py
@@ -1,3 +1,5 @@
+#BOJ_15649.py와 매우 유사한 문제로 차이점은 숫자를 중복해서 선택할 수 있음
+#BOJ_15649.py와 달리 중복이 가능하므로 숫자의 사용 여부를 체크하는 check 리스트 불필요
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
