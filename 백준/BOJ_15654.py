4a600668df18cd87db385b77c71fe87fb236e347
je488 <134580295+je488@users.noreply.github.com>
2024-03-06 16:16:10 +0900

    설명 추가

--- 백준/BOJ_15654.py
+++ 백준/BOJ_15654.py
@@ -1,5 +1,6 @@
 #BOJ_15649.py와 유사한 문제
 #BOJ_15649.py와 달리 1 ~ N중에 M을 고르는 것이 아닌 입력으로 주어지는 N개의 수 중에 M개 고르기
+#수열은 사전 순으로 증가하는 순서로 출력해야 하므로 N개의 수가 들어있는 a를 오름차순으로 정렬
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
