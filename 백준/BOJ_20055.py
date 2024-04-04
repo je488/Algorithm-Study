4b751d372f88955ece871e45b13d87e42971658a
je488 <pje488@naver.com>
2024-04-04 12:28:48 +0900

    성공

---
+++ 백준/BOJ_20055.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+box = [False] * (2*n)
+zero = 0
+t = 1
+while True:
+    a = a[-1:] + a[:-1]
+    box = box[-1:] + box[:-1]
+    if box[n-1]:
+        box[n-1] = False
+    for i in range(n-2, -1, -1):
+        if box[i]:
+            if box[i+1] == False and a[i+1] > 0:
+                box[i] = False
+                box[i+1] = True
+                a[i+1] -= 1
+                if a[i+1] == 0:
+                    zero += 1
+    if box[n-1]:
+        box[n-1] = False
+    if a[0] > 0:
+        box[0] = True
+        a[0] -= 1
+        if a[0] == 0:
+            zero += 1
+    if zero >= k:
+        print(t)
+        break
+    t += 1
