d757257a3f74506f9c2cf28771c47f3ee1e24fd8
je488 <134580295+je488@users.noreply.github.com>
2024-07-16 16:30:15 +0900

    설명 추가

--- 백준/BOJ_16967.py
+++ 백준/BOJ_16967.py
@@ -1,3 +1,8 @@
+#1 <= X, 1 <= Y 이므로 A[1][1]은 B[1][1]과 같다
+#B[X+1][Y+1] = A[X+1][Y+1] + A[1][1]
+#A[X+1][Y+1] = B[X+1][Y+1] - A[1][1]
+#위의 식을 이용하여 원래 배열의 값 구하기
+#코드에서는 원래 배열을 따로 만들지 않고 값만 변경한 후 H * W의 크기만큼만 출력
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
