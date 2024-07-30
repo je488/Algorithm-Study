3af61928f101e1b654f04d75d4796bd0cf6ea5ce
je488 <134580295+je488@users.noreply.github.com>
2024-07-30 14:37:37 +0900

    설명 추가

--- SWEA_최대조작횟수.py
+++ SWEA_최대조작횟수.py
@@ -1,3 +1,11 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYj_Dz-6qLgDFASl(문제링크)
+#a는 더하기만 가능하고 b는 빼기만 가능하므로 a가 b보다 큰 경우 조작 불가능
+#sub이 0이면 a, b는 같으므로 조작 횟수는 0
+#sub이 1이거나 음수이면 조작을 통해 a와 b를 같게 할 수 없으므로 -1
+#sub이 짝수이면 가장 작은 소수인 2를 더하거나 빼주면 되므로 최대 조작 횟수는 sub // 2
+#sub이 홀수이면 2를 제외한 가장 작은 소수인 3을 빼서 짝수를 만들고 2를 더하거나 빼기
+#따라서 최대 조작 횟수는 (sub - 3) // 2
+#최대 조작 횟수를 구해야 하므로 가장 작은 소수인 2와 2를 제외한 가장 작은 소수인 3 이용
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
