87cf9081747601478dabe6304bac150b062aee0d
je488 <pje488@naver.com>
2024-07-30 13:17:09 +0900

    성공

--- 백준/BOJ_14226.py
+++ 백준/BOJ_14226.py
@@ -3,24 +3,24 @@ from collections import deque
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 n = int(input())
-dist = [[-1] * (n+1) for _ in range(n+1)]
+sec = [[-1] * (n+1) for _ in range(n+1)]
 Q = deque()
 Q.append((1, 0))
-dist[1][0] = 0
+sec[1][0] = 0
 while Q:
     s, c = Q.popleft()
-    if dist[s][s] == -1:
-        dist[s][s] = dist[s][c] + 1
+    if sec[s][s] == -1:
+        sec[s][s] = sec[s][c] + 1
         Q.append((s, s))
-    if s+c <= n and dist[s+c][c] == -1:
-        dist[s+c][c] = dist[s][c] + 1
+    if s+c <= n and sec[s+c][c] == -1:
+        sec[s+c][c] = sec[s][c] + 1
         Q.append((s+c, c))
-    if s-1 >= 0 and dist[s-1][c] == -1:
-        dist[s-1][c] = dist[s][c] + 1
+    if s-1 >= 0 and sec[s-1][c] == -1:
+        sec[s-1][c] = sec[s][c] + 1
         Q.append((s-1, c))
 ans = -1
 for i in range(n+1):
-    if dist[n][i] != -1:
-        if ans == -1 or ans > dist[n][i]:
-            ans = dist[n][i]
+    if sec[n][i] != -1:
+        if ans == -1 or ans > sec[n][i]:
+            ans = sec[n][i]
 print(ans)
