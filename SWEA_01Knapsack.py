1282066dbbe46bd62fc6e01bea82399c43ae83ef
je488 <134580295+je488@users.noreply.github.com>
2024-08-01 13:45:03 +0900

    설명 추가

--- SWEA_01Knapsack.py
+++ SWEA_01Knapsack.py
@@ -1,3 +1,15 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWBJAVpqrzQDFAWr(문제링크)
+#n <= 100이므로 백트래킹을 이용하면 시간복잡도가 O(2^n) = 2^100으로 1억보다 크기 때문에 시간 초과
+#따라서 다이나믹 프로그래밍을 이용하여 해결
+#dp[i][j] : i번째 물건까지 고려하고 가방의 부피가 j일 때 최대가 되는 가치의 합
+#현재 물건의 부피가 v일 때 v > j인 경우 현재 물건을 가방에 넣을 수 없음
+#따라서 dp[i][j] = dp[i-1][j] (이전 물건까지 고려하고 가방 부피가 j인 최대 가치 합)
+#현재 물건의 부피가 v일 때 v <= j인 경우 현재 물건을 가방에 넣을지 말지 결정
+#현재 물건을 가방에 넣는 경우: dp[i][j] = dp[i-1][j-v] + c
+#dp[i-1][j-v] + c : 이전 물건까지 고려하고 가방 부피가 j-v일 때 최대 가치 합 + 현재 물건의 가치
+#현재 물건을 가방에 넣지 않는 경우: dp[i][j] = dp[i-1][j]
+#가방에 넣는 경우와 넣지 않는 경우 중 최대 가치의 합이 더 큰 것을 dp[i][j]에 넣기
+#따라서 dp[i][j] = max(dp[i-1][j], dp[i-1][j-v] + c)
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
