77a2cb27864319af519e347487e0b8d55e410183
je488 <pje488@naver.com>
2024-08-21 17:33:27 +0900

    성공

--- 백준/BOJ_7576.py
+++ 백준/BOJ_7576.py
@@ -6,26 +6,28 @@ dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 m, n = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
+day = [[-1] * m for _ in range(n)]
 Q = deque()
-dist = [[-1] * m for _ in range(n)]
 for i in range(n):
     for j in range(m):
         if a[i][j] == 1:
-            dist[i][j] = 0
+            day[i][j] = 0
             Q.append((i, j))
+
 while Q:
     x, y = Q.popleft()
     for d in range(4):
         xx = x + dx[d]
         yy = y + dy[d]
         if 0 <= xx < n and 0 <= yy < m:
-            if a[xx][yy] == 0 and dist[xx][yy] == -1:
+            if a[xx][yy] == 0 and day[xx][yy] == -1:
+                day[xx][yy] = day[x][y] + 1
                 Q.append((xx, yy))
-                dist[xx][yy] = dist[x][y] + 1
 
-ans = max([max(row) for row in dist])
+ans = max([max(row) for row in day])
 for i in range(n):
     for j in range(m):
-        if a[i][j] == 0 and dist[i][j] == -1:
+        if a[i][j] == 0 and day[i][j] == -1:
             ans = -1
+            break
 print(ans)
