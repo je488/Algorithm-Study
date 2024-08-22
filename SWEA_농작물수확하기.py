c38102c2f51b4dabea40e91d3e10ca1e916812b3
je488 <134580295+je488@users.noreply.github.com>
2024-08-22 16:43:23 +0900

    설명 추가

--- SWEA_농작물수확하기.py
+++ SWEA_농작물수확하기.py
@@ -1,3 +1,9 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7GLXqKAWYDFAXB(문제링크)
+#s, e는 특정 행에서 더해야 하는 범위의 시작점과 끝점
+#행마다 s부터 e까지의 값을 ans에 더하기
+#행을 기준으로 중간까지는 s, e의 간격이 벌어지고 중간 이후부터는 s, e의 간격이 다시 좁아짐
+#따라서 행이 n//2(행의 중간 인덱스)보다 작은 경우 s -= 1, e += 1 -> 시작점과 끝점의 간격 넓히기
+#행이 n//2보다 크거나 같은 경우 s += 1, e -= 1 -> 시작점과 끝점의 간격 좁히기
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
