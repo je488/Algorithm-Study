f630a2f2ab52599135135ff29b119018810ded65
je488 <134580295+je488@users.noreply.github.com>
2024-09-02 14:54:51 +0900

    설명 추가

--- SWEA_원재의벽꾸미기.py
+++ SWEA_원재의벽꾸미기.py
@@ -1,3 +1,9 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV2b9AkKACkBBASw(문제링크)
+#1 * 1 타일 n개를 이용하여 r * c 크기의 직사각형 타일을 만들어야 하므로 r * c <= n
+#r = 1, c = 2일 때와 r = 2, c = 1일 때의 tmp값은 같으므로 c는 1이 아닌 r부터 반복
+#r * c <= n인 경우에만 tmp값을 구하므로 tmp는 항상 0 이상
+#따라서 정답(ans)을 -1로 초기화한 뒤 가장 먼저 구한 tmp값을 ans에 할당
+#이후로는 이전에 구한 ans와 tmp를 비교해서 더 작은 값을 ans에 할당하여 최솟값 구하기
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
