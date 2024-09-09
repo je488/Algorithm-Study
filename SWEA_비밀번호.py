86a9330b63f0a178d7cdd865f03bb57ac573fb05
je488 <134580295+je488@users.noreply.github.com>
2024-09-09 13:02:25 +0900

    설명 추가

--- SWEA_비밀번호.py
+++ SWEA_비밀번호.py
@@ -2,7 +2,7 @@
 #번호가 같으면 계속해서 소거하기 위해 스택 이용
 #a를 하나씩 탐색하면서 ans가 비어있거나 ans의 top이 a의 번호와 다르면 ans에 push
 #ans의 top이 a의 번호와 같으면 소거해야 하므로 ans에서 pop
-#ans의 top과 번호를 비교하므로 번호 쌍을 소거하고 소거된 번호 쌍의 좌우 번호가 같으면 또 소거 가능
+#ans의 top과 번호를 비교하므로 번호 쌍을 소거하고 소거된 번호 쌍의 좌우가 같으면 또 소거 가능
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
