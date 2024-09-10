41bba6b78d6b78066f25eeceb9be96dbd0a6cacd
je488 <134580295+je488@users.noreply.github.com>
2024-09-10 15:08:07 +0900

    설명 추가

--- SWEA_최대상금.py
+++ SWEA_최대상금.py
@@ -1,3 +1,12 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15Khn6AN0CFAYD(문제링크)
+#숫자판의 최대 자릿수는 6자리이고 최대 교환 횟수는 10번이므로 모든 경우의 수는 6C2^10
+#따라서 모든 경우를 다 해보면 시간초과가 발생할 수 있으므로 중복되는 경우는 제외하고 완전 탐색
+#a는 숫자판, n은 교환 횟수
+#now에는 교환하기 전, nxt에는 교환한 후의 숫자판을 저장하고 중복을 제거하기 위해 set() 이용
+#n번 교환해야 하므로 n만큼 반복하고 now가 비어있을 때까지 반복하며 숫자 교환
+#1번 교환하고 나면 다음 교환을 위해 now에는 nxt, nxt에는 비어있는 set 할당
+#n번을 교환한 뒤 가능한 모든 경우의 수는 now에 저장되어 있으므로 now에서 최댓값 찾기
+#참고 : https://happybplus.tistory.com/352
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
