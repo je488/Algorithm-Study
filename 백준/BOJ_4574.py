8af300e8176ab51db3fa6010d39c38850c4aa473
je488 <134580295+je488@users.noreply.github.com>
2024-10-03 13:26:25 +0900

    설명 추가

--- 백준/BOJ_4574.py
+++ 백준/BOJ_4574.py
@@ -1,3 +1,22 @@
+#go(z)는 z번째 칸을 채우는 함수
+#z는 row-major order로 번호를 매김 -> (x, y)의 경우, z = 9*x + y
+#z로는 문제를 풀기 어려우므로 z를 x행 y열로 바꿔주기
+#c[i][j]는 i행에 숫자 j가 있으면 True, c2[i][j]는 i열에 숫자 j가 있으면 True
+#c3[i][j]는 i번째 작은 정사각형에 숫자 j가 있으면 True
+#domino는 도미노의 중복 사용 여부를 체크하는 리스트
+#스도쿠와 달리 스도미노쿠는 z번째 칸의 오른쪽 또는 아래에도 수를 넣어야 하므로 dx, dy 필요
+#convert(s)는 s를 x행 y열의 형태로 바꿔주는 함수
+#square(x, y)는 (x, y)가 몇 번째 작은 정사각형에 속해있는지 구해주는 함수
+#can(x, y)는 x행 y열에 num이 올 수 있는지 검사하는 함수(c, c2, c3 이용)
+#check(x, y, num, what)은 x행 y열에 what의 값에 따라 num을 놓거나 놓지 않음을 표시해주는 함수
+#check_range(x, y)는 x행 y열이 보드의 범위를 넘어가지 않는지 검사하는 함수
+#(nx, ny)는 현재 칸(x, y)을 기준으로 오른쪽 또는 아래 칸
+#(nx, ny)가 보드의 범위를 넘어가거나 (nx, ny)칸에 숫자가 이미 있으면 continue
+#1 ~ 9까지 이중 for문을 이용하여 도미노 만들기
+#i == j이거나 domino[i][j] 값이 True이면(이미 그 도미노를 사용한 경우) continue
+#can을 이용하여 (x, y)칸에 i, (nx, ny)칸에 j가 올 수 있는지 검사
+#올 수 있으면 check를 이용하여 도미노를 놓고 domino와 a의 값을 바꾼 후(준비) go 함수 호출
+#함수를 호출한 뒤에는 check를 이용하여 도미노를 삭제하고 domino와 a의 값 원래대로 바꾸기(복원)
 import sys
 sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
