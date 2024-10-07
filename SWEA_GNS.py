ff1e602bd5d4f3278e2a16a2a257d52178b6937f
je488 <134580295+je488@users.noreply.github.com>
2024-10-07 12:51:09 +0900

    설명 추가

--- SWEA_GNS.py
+++ SWEA_GNS.py
@@ -1,3 +1,7 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14jJh6ACYCFAYD(문제링크)
+#입력받은 문자열 s에서 ZRO부터 NIN까지 순서대로 개수를 세서 cnt에 저장
+#cnt[0]은 s에서 ZRO의 개수, cnt[1]은 s에서 ONE의 개수, ..., cnt[9]는 s에서 NIN의 개수
+#cnt를 바탕으로 ZRO부터 NIN까지 순서대로 개수만큼 출력
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
