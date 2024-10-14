75227a8b4d919cc410c5b5748040ec3955900663
je488 <134580295+je488@users.noreply.github.com>
2024-10-14 13:57:12 +0900

    설명 추가

--- SWEA_Magnetic.py
+++ SWEA_Magnetic.py
@@ -1,3 +1,9 @@
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14hwZqABsCFAYD(문제링크)
+#board의 열을 탐색하면서 N극(1), S극(2) 짝의 개수 찾기
+#flag를 이용하여 board의 값이 1이면 flag값을 True로 바꾸기
+#board의 값이 2이고 flag의 값이 True이면 교착상태이므로 res에 1 더하고 flag값을 원래대로 바꾸기
+#N극이 테이블 위에 있으므로 값이 2(S극)인 것이 값이 1인 것보다 먼저 발견되면 테이블 아래로 떨어짐
+#따라서 값이 1인 것을 먼저 찾은 뒤에 값이 2인 것 찾기
 import sys
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
